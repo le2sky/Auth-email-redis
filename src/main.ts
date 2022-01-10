b6789fe758deedcbe513be5d384fbc27cf256464
leesky <leehaneul0623@gmail.com>
2022-01-10 19:09:44 +0900

    [main.ts] 싱글톤 리팩토링

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { WinstonModule } from 'nest-winston';
@@ -9,52 +9,117 @@ import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as expressBasicAuth from 'express-basic-auth';
 
-async function bootstrap() {
-  const config = new DocumentBuilder()
-    .setTitle('email-demo-openApi')
-    .setDescription(
-      'Nestjs 이메일 인증 기반 회원가입 서비스 오픈 API 문서입니다.',
-    )
-    .setVersion('1.0')
-    .addTag('email-demo')
-    .build();
+const NEST_LOGGER = {
+  logger: WinstonModule.createLogger({
+    transports: [
+      new winston.transports.Console({
+        level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          nestWinstonModuleUtilities.format.nestLike('email-demo', {
+            prettyPrint: true,
+          }),
+        ),
+      }),
+    ],
+  }),
+};
 
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        new winston.transports.Console({
-          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            nestWinstonModuleUtilities.format.nestLike('email-demo', {
-              prettyPrint: true,
-            }),
-          ),
-        }),
-      ],
-    }),
-  });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  app.useGlobalFilters(new HttpExceptionFilter());
+class Application {
+  private corsOriginList: string[];
+  private SWAGGER_USER: string;
+  private SWAGGER_PASSWORD: string;
+  private PORT: string;
+  private MODE: string;
+  private logger = NEST_LOGGER.logger;
 
-  app.use(
-    ['/docs', 'docs-json'],
-    expressBasicAuth({
-      challenge: true,
-      users: {
-        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
-      },
-    }),
-  );
+  constructor(private server: NestExpressApplication) {
+    this.server = server;
+    this.PORT = process.env.PORT || '3000';
+    this.corsOriginList = process.env.CORS_ORIGIN_LIST
+      ? process.env.CORS_ORIGIN_LIST.split(',').map((origin) => origin.trim())
+      : ['*'];
+    this.SWAGGER_USER = process.env.SWAGGER_USER || 'admin';
+    this.SWAGGER_PASSWORD = process.env.SWAGGER_PASSWORD || '1234';
+    this.MODE = process.env.NODE_ENV;
+  }
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  private setUpBasicAuth() {
+    this.server.use(
+      ['/docs', 'docs-json'],
+      expressBasicAuth({
+        challenge: true,
+        users: {
+          [this.SWAGGER_USER]: this.SWAGGER_PASSWORD,
+        },
+      }),
+    );
+  }
 
-  const PORT = process.env.PORT;
-  await app.listen(PORT);
+  private setUpOpenAPImidleware() {
+    SwaggerModule.setup(
+      'docs',
+      this.server,
+      SwaggerModule.createDocument(
+        this.server,
+        new DocumentBuilder()
+          .setTitle('email-demo-openApi')
+          .setDescription(
+            'Nestjs 이메일 인증 기반 회원가입 서비스 오픈 API 문서입니다.',
+          )
+          .setVersion('1.0')
+          .addTag('email-demo')
+          .build(),
+      ),
+    );
+  }
+
+  private async setUpGlobalMiddleware() {
+    this.server.enableCors({
+      origin: this.corsOriginList,
+      credentials: true,
+    });
+    this.setUpBasicAuth();
+    this.setUpOpenAPImidleware();
+    this.server.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+    );
+    this.server.useGlobalFilters(new HttpExceptionFilter());
+  }
+
+  async bootstrap() {
+    await this.setUpGlobalMiddleware();
+    await this.server.listen(this.PORT);
+  }
+
+  startLog() {
+    this.logger.log(
+      `✅ ${this.MODE} 모드로 서버-사이드 어플리케이션이 실행됩니다. `,
+    );
+    if (this.MODE === 'development') {
+      this.logger.log(`✅ Server on http://localhost:${this.PORT}`);
+    } else {
+      this.logger.log(`✅ Server on port ${this.PORT}...`);
+    }
+  }
+
+  errorLog(error: string) {
+    this.logger.error(`🆘 Server error ${error}`);
+  }
 }
-bootstrap();
+
+async function init(): Promise<void> {
+  const server = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    NEST_LOGGER,
+  );
+  const app = new Application(server);
+  await app.bootstrap();
+  app.startLog();
+}
+
+init().catch((error) => {
+  new Logger('init').error(error);
+});
