1427826ca2eba8fb8a8b4857f862319a160ef370
leesky <leehaneul0623@gmail.com>
2022-01-10 19:21:39 +0900

    [joi] env 스키마 유효성검사 추가

--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -1,3 +1,20 @@
 import * as Joi from 'joi';
 
-export const validationSchema = Joi.object({});
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'stage')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+  EMAIL_SERVICE: Joi.string().required(),
+  EMAIL_HOST: Joi.string().required(),
+  EMAIL_AUTH_USER: Joi.string().required(),
+  EMAIL_AUTH_PASSWORD: Joi.string().required(),
+  EMAIL_BASE_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPRIRE_IN: Joi.string().required(),
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
+  SWAGGER_USER: Joi.string().required(),
+  SWAGGER_PASSWORD: Joi.string().required(),
+  IMG_HOST_URL: Joi.string().required(),
+});
